44d8c2c7dd3d60fee3addb4f63277002bef40283
ghostrider-reborn <adithya.r02@outlook.com>
2021-08-22 16:37:18 +0800

    sdm660-common: Disable dexpreopt minidebuginfo
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Change-Id: Ief843fc411189906c5d0183a6ff4485f6b3041ee
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -431,7 +431,12 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+USE_DEX2OAT_DEBUG := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+WITH_DEXPREOPT_DEBUG_INFO := false
 
 # Vibrator
 PRODUCT_PACKAGES += \
